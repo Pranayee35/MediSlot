b80312a276b9aa313ab867b022c3227adf7a851a
Pranayee35 <pranayeegunda@gmail.com>
2026-01-14 18:06:43 +0530

    Design About page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-
+import about_image from '../assets/about_image.png'
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-90' src={about_image}/>
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Welcome to MediSlot,your trudted parrtner in managing your healthcare needs conveniently and efficiently.At MediSlot,we understand the challenges individuals face when it comes to scheduling doctor appointments and managing health records.</p>
+          <p>MediSlot is committed to excellence in healthcare technology,we continuously strive to enhance our platform,integrating the latest advancements to improve user experience and deliver superior service.Whether you are booking your first appointment or managing ongoing care,MediSlot is heare to support you every step of the way.</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Our vision at MediSlot is to create a seamless healthcare experience for every user.We aim to bridge the gap betweeen patients and healthcare providers,making it easier for you to access the care you need,when you need it.</p>
+        </div>
+      </div>
+      <div className='text-xl my-4'>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+      </div>
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Streamlined appointment scheduling that fits into yourbusy lifestyle</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+         <b>Convenience:</b>
+         <p>Access to a network of trusted healthcare proffessionals in your area</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Tailored recommendations and reminders to help you stay on top of your health</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
