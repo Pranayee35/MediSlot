ecfcb08c13b4ba370e7938afeb10492d3bf5f252
Pranayee35 <pranayeegunda@gmail.com>
2026-01-15 13:44:20 +0530

    design login page

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import profile_pic from '../assets/profile_pic.png'
 const MyProfile = () => {
+  const [userData,setUserData] = useState({
+    name:'Edward Vincent',
+    image:profile_pic,
+    email:'richardjameswap@gmail.com',
+    phone:'+1 123 456 7890',
+    address:{
+      line1:'57th cross,richmond',
+      line2:'circle,church road,London'
+  },
+    gender:'Male',
+    dob:'2000-01-20'
+  });
+  const [isEdit,setIsEdit] = useState(true);
   return (
-    <div>MyProfile</div>
+    <div>
+      <img src={userData.image} alt='image'/>
+      {
+        isEdit
+        ?<input type='text' value={userData.nmae} onChange={e=>setUserData(prev=>({...prev,name:e.target.value}))}/>
+        :<p>{userData.name}</p>
+      }
+    </div>
   )
 }
 
