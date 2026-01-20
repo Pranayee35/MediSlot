51ac400ef3a70b80b9bf530a33ec7fdaa1b374ea
Pranayee35 <pranayeegunda@gmail.com>
2026-01-20 17:34:37 +0530

    implement admin logout by clearing token and redirecting

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import { useContext } from 'react'
 import { AdminContext } from '../context/AdminContext'
+import { useNavigate } from 'react-router-dom'
 const Navbar = () => {
-    const {atoken} = useContext(AdminContext)
+    const {atoken,setAtoken} = useContext(AdminContext)
+
+    const navigate = useNavigate()
+
+    const logout = ()=>{
+
+        navigate('/')
+        atoken&&setAtoken('')
+        atoken && localStorage.removeItem('atoken')
+    }
+
   return (
-    <div className='flex justify-between items-center border-b bg-white'>
+    <div className='flex justify-between items-center border-b bg-white p-5'>
         <div className='flex items-center text-xs gap-2'>
-            <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo}/>
+            <img className='w-36 sm:w-40 cursor-pointer' src={assets.medislot}/>
             <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>
                 {
                     atoken? 'Admin'
@@ -15,7 +26,7 @@ const Navbar = () => {
                 }
             </p>
         </div>
-        <button className='bg-[#5f6fff] text-white text-sm px-10 py-2 rounded-full'>Logout</button>
+        <button onClick={logout} className='bg-[#5f6fff] text-white text-sm px-10 py-2 rounded-full'>Logout</button>
     </div>
   )
 }
