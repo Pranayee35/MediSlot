38c03be425f3f8dbee40d0e46b6be8f02e4508f4
Pranayee35 <pranayeegunda@gmail.com>
2026-01-22 21:58:47 +0530

    Integrate doctor api in frontend

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -14,7 +14,7 @@ const DoctorsList = () => {
       <div className='w-full flex flex-wrap gap-4 pt-5 gap-y-6'>
     {doctors?.length>0&& doctors.map((item,index)=>(
         <div className='border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer' key={index}>
-          <img className='bg-indigo-50 group-hover:bg-primary transition-all duration-500' src={item.image} alt='image'/>
+          <img className='bg-indigo-50 hover:bg-[#5f6fff] transition-all duration-500' src={item.image} alt='image'/>
           <div className='p-4'>
             <p className='text-neutral-800 text-lg font-medium'>{item.name}</p>
             <p className='text-zinc-600 text-sm'>
